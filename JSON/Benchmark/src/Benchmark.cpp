ce1893842726eb0f12d94617cc765f53403a40be
WontonSkin <724011024@qq.com>
2020-12-05 03:33:26 -0500

    modify Benchmark.cpp

--- JSON/Benchmark/src/Benchmark.cpp
+++ JSON/Benchmark/src/Benchmark.cpp
@@ -27,7 +27,8 @@ int main(int argc, char** argv)
 {
 	Poco::Stopwatch sw;
 
-	std::string dir = Poco::Environment::get("POCO_BASE") + "/JSON/samples/Benchmark/";
+	//export POCO_BASE="/root/shareDir/poco-sample"
+	std::string dir = Poco::Environment::get("POCO_BASE") + "/JSON/Benchmark/";
 	Poco::Path filePath(dir, "input.big.json");
 
 	std::ostringstream ostr;
